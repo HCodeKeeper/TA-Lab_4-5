850940dc8f0925e989a6ff0d949bbb638445abd7
Max Ivannikov <58997690+HCodeKeeper@users.noreply.github.com>
2022-04-17 19:53:18 +0300

    added commented out tests for merge sort

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from InsertionSort import InsertionSort
 from SelectionSort import SelectionSort
 from SortContext import SortContext
 from ShellSort import ShellSort
+import merge_sort
 
 random_lists = [
     [random.randrange(1, 1000) for i in range(1000)],
@@ -67,3 +68,28 @@ do_tests()
 print(colored("SHELL SORT", 'red'))
 sorter.set_strategy(shell_sort)
 do_tests()
+
+'''merge test
+k100 = [random.randrange(1, 100_000) for _ in range(100_000)]
+kk = [random.randrange(1, 1_000_000) for _ in range(1_000_000)]
+sk100 = [v+1 for v in range(100_000)]
+skk = [v+1 for v in range(1_000_000)]
+
+k100_c = k100.copy()
+kk_c = kk.copy()
+sk100_c = sk100.copy()
+skk_c = skk.copy()
+
+t = time.perf_counter()
+sort(k100_c)
+print((time.perf_counter() - t) * 1000)
+t = time.perf_counter()
+sort(kk_c)
+print((time.perf_counter() - t) * 1000)
+t = time.perf_counter()
+sort(sk100_c)
+print((time.perf_counter() - t) * 1000)
+t = time.perf_counter()
+sort(skk_c)
+print((time.perf_counter() - t) * 1000)
+```
